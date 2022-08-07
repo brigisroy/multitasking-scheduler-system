ad38bff9236eef629984e11c1cc856aaea112542
brigis_roy <brigisroy007@gmail.com>
2022-08-07 12:45:46 +0530

    db file moved

--- server/src/main/java/hell/fire/worker/controller/JobsController.java
+++ server/src/main/java/hell/fire/worker/controller/JobsController.java
@@ -8,18 +8,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("jobs")
+@RequestMapping("apis")
 public class JobsController {
 
     @Autowired
     private JobService jobService;
 
-    @GetMapping("/")
+    @GetMapping("/jobs")
     public ResponseEntity<Object> hello(){
         return new ResponseEntity<>(jobService.getAllJobs(), HttpStatus.OK);
     }
 
-    @PostMapping("/")
+    @PostMapping("/jobs")
     public ResponseEntity<Object> createJob(@RequestBody JobsDTO jobsDTO){
      return new ResponseEntity<>(jobService.createJob(jobsDTO),HttpStatus.OK);
     }
