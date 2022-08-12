fb5638df77dc9486407e6b2f7cca17622c096b91
brigis_roy <brigisroy007@gmail.com>
2022-08-12 23:54:30 +0530

    code

--- server/src/main/java/hell/fire/worker/controller/JobsController.java
+++ server/src/main/java/hell/fire/worker/controller/JobsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("apis")
+@RequestMapping("api")
 public class JobsController {
 
     @Autowired
